b2de24b60d8210c8e69510b0442ba4fdbc0e5cb8
Rahul <rahul.241226@ncit.edu.np>
2026-09-03 14:13:26 +0545

    icons

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ window.config(bg='white')
 
 
 #adding image and resize as you choice
-image = Image.open("checklist.png")
+image = Image.open("icons/checklist.png")
 image = image.resize((64, 64))
 bg_Image = ImageTk.PhotoImage(image)
 
@@ -42,7 +42,7 @@ leftframe = Frame(window)
 leftframe.place(x=0,y=102,width=200,height=790)
 
 #logoimage
-logoimage = Image.open("list.png")
+logoimage = Image.open("icons/list.png")
 logoimage = logoimage.resize((64, 64))
 logoimage = ImageTk.PhotoImage(logoimage)
 imageLabel = Label(leftframe, image=logoimage)
@@ -50,7 +50,7 @@ imageLabel.pack()
 
 menulabel= Label(leftframe,text='Menu',font=('times new roman',20),bg='#009688')
 menulabel.pack(fill=X)
-employee_icon = Image.open("employee.png")
+employee_icon = Image.open("icons/employee.png")
 employee_icon = employee_icon.resize((24, 24))
 employee_icon = ImageTk.PhotoImage(employee_icon)
 
@@ -64,7 +64,7 @@ employee_button = Button(
 employee_button.pack(fill=X)
 
 
-supplier_icon = Image.open("supplier.png")
+supplier_icon = Image.open("icons/supplier.png")
 supplier_icon = supplier_icon.resize((24, 24))
 supplier_icon = ImageTk.PhotoImage(supplier_icon)
 
@@ -80,7 +80,7 @@ supplier_button = Button(
 supplier_button.pack(fill=X)
 
 
-category_icon = Image.open("catagory.png")
+category_icon = Image.open("icons/catagory.png")
 category_icon = category_icon.resize((24, 24))
 category_icon = ImageTk.PhotoImage(category_icon)
 
@@ -96,7 +96,7 @@ category_button = Button(
 category_button.pack(fill=X)
 
 
-product_icon = Image.open("product.png")
+product_icon = Image.open("icons/product.png")
 product_icon = product_icon.resize((24, 24))
 product_icon = ImageTk.PhotoImage(product_icon)
 
@@ -112,7 +112,7 @@ padx=10
 product_button.pack(fill=X)
 
 
-sales_icon = Image.open("sales.png")
+sales_icon = Image.open("icons/sales.png")
 sales_icon = sales_icon.resize((24, 24))
 sales_icon = ImageTk.PhotoImage(sales_icon)
 
@@ -129,7 +129,7 @@ sales_button = Button(
 sales_button.pack(fill=X)
 
 
-exit_icon = Image.open("exit.png")
+exit_icon = Image.open("icons/exit.png")
 exit_icon = exit_icon.resize((24, 24))
 exit_icon = ImageTk.PhotoImage(exit_icon)
 
@@ -152,7 +152,7 @@ emp_frame = Frame(window,bg='#2C3E50',bd=3,relief=RIDGE)
 emp_frame.place(x=400,y=125,width=300,height=200)
 
 
-emp_icon = Image.open("emp.png")
+emp_icon = Image.open("icons/emp.png")
 emp_icon = emp_icon.resize((80, 80))
 emp_icon = ImageTk.PhotoImage(emp_icon)
 emp_icon_label = Label(emp_frame, image=emp_icon,bg='#2C3E50')
@@ -173,7 +173,7 @@ supp_frame = Frame(window,bg='#8E44AD',bd=3,relief=RIDGE)
 supp_frame.place(x=800,y=125,width=300,height=200)
 
 
-supp_icon = Image.open("suppliers.png")
+supp_icon = Image.open("icons/suppliers.png")
 supp_icon = supp_icon.resize((80, 80))
 supp_icon = ImageTk.PhotoImage(supp_icon)
 supp_icon_label = Label(supp_frame, image=supp_icon,bg='#8E44AD')
@@ -195,7 +195,7 @@ cat_frame = Frame(window,bg='#27AE60',bd=3,relief=RIDGE)
 cat_frame.place(x=1200,y=125,width=300,height=200)
 
 
-cat_icon = Image.open("catagory.png")
+cat_icon = Image.open("icons/catagory.png")
 cat_icon = cat_icon.resize((80, 80))
 cat_icon = ImageTk.PhotoImage(cat_icon)
 cat_icon_label = Label(cat_frame, image=cat_icon,bg='#27AE60')
@@ -217,7 +217,7 @@ prod_frame = Frame(window,bg='#2C3E50',bd=3,relief=RIDGE)
 prod_frame.place(x=400,y=525,width=300,height=200)
 
 
-prod_icon = Image.open("product.png")
+prod_icon = Image.open("icons/product.png")
 prod_icon = prod_icon.resize((80, 80))
 prod_icon = ImageTk.PhotoImage(prod_icon)
 prod_icon_label = Label(prod_frame, image=prod_icon,bg='#2C3E50')
@@ -236,7 +236,7 @@ sale_frame = Frame(window,bg='#E74C3C',bd=3,relief=RIDGE)
 sale_frame.place(x=800,y=525,width=300,height=200)
 
 
-sale_icon = Image.open("sales.png")
+sale_icon = Image.open("icons/sales.png")
 sale_icon = sale_icon.resize((80, 80))
 sale_icon = ImageTk.PhotoImage(sale_icon)
 sale_icon_label = Label(sale_frame, image=sale_icon,bg='#E74C3C')
